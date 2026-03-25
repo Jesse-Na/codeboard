e803877440ac3976c2811206be6f86c17c166464
erinez <czhu233@gmail.com>
2026-03-25 12:47:37 -0400

    UI for room deletion

---
+++ components/my-rooms/room-delete.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { TrashIcon } from "lucide-react"
+
+type deleteRoomProp= {
+    onDelete: () => void;
+    children: React.ReactNode;
+}
+
+export function RoomDeletion({onDelete, children}: deleteRoomProp ) {
+    return (
+    <ContextMenu>
+        <ContextMenuTrigger>
+            {children}
+        </ContextMenuTrigger>
+
+      <ContextMenuContent>
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <ContextMenuItem onSelect={(e) => e.preventDefault()}>
+                <TrashIcon className="mr-2 h-4 w-4" />
+                    <span>Delete Room</span>
+                </ContextMenuItem>
+            </AlertDialogTrigger>
+            
+            <AlertDialogContent size="sm">
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Delete room?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This will permanently delete the room.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+
+                <AlertDialogFooter>
+                    <AlertDialogCancel variant="outline">Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onDelete} variant="destructive">Delete</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+
+      </ContextMenuContent>
+    </ContextMenu>
+
+    )
+}
